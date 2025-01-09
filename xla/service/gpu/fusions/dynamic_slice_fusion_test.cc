openxla/xla

Add support for async dynamic slice fusion
This patch adds async handling to dynamic slice fusion when the hero
operation is a collective operation. Currently, only reduce-scatter is
supported as a hero operation in dynamic slice thunk, so this patch also
follows the same.

Added a test with compute, to ensure that communication and compute
overlap in the thunks emitted.

xla/service/gpu/fusions/dynamic_slice_fusion_test.cc
@@ -3131,9 +3131,9 @@ TEST_F(DynamicSliceFusionTest, ReduceScatterSlice) {
   TF_ASSERT_OK_AND_ASSIGN(
       auto exec, CreateExecutable(std::move(module_new_opt_clone), false));
   GpuExecutable* gpu_exec = dynamic_cast<GpuExecutable*>(exec.get());
-  ASSERT_EQ(gpu_exec->GetThunk().thunks().size(), 2ul);
-  auto& rs_start_thunk = gpu_exec->GetThunk().thunks()[0];
-  auto& rs_done_thunk = gpu_exec->GetThunk().thunks()[1];
+  ASSERT_EQ(gpu_exec->GetThunk().thunks().size(), 4ul);
+  auto& rs_start_thunk = gpu_exec->GetThunk().thunks()[1];
+  auto& rs_done_thunk = gpu_exec->GetThunk().thunks()[2];
   ASSERT_EQ(rs_start_thunk->kind(), Thunk::kNcclReduceScatterStart);
   ASSERT_EQ(rs_done_thunk->kind(), Thunk::kNcclReduceScatterDone);
 
@@ -3187,6 +3187,91 @@ TEST_F(DynamicSliceFusionTest, ReduceScatterDynamicSlice) {
   ErrorSpec error{/*aabs=*/1e-3, /*arel=*/1e-3};
   EXPECT_TRUE(RunAndCompareTwoModulesReplicated(std::move(module_ref_opt),
                                                 std::move(module_new_opt),
+                                                false, true, error));
+}
+
+TEST_F(DynamicSliceFusionTest, AsyncCollective) {
+  const char* hlo = R"(
+    HloModule test, replica_count=2
+    add {
+      x = s32[] parameter(0)
+      y = s32[] parameter(1)
+      ROOT add = s32[] add(x, y)
+    }
+    ENTRY main {
+      destination = s32[2,2,32] parameter(0)
+      c1 = s32[] constant(1)
+      c0 = s32[] constant(0)
+      c4 = s32[] constant(4)
+      source = s32[8,32] parameter(1)
+      a = s32[1024,1024] parameter(2)
+      b = s32[1024,1024] parameter(3)
+      slice = s32[4,32] slice(source), slice={[4:8], [0:32]}
+      rs = s32[2,32] reduce-scatter(slice), replica_groups={{0,1}}, dimensions={0}, to_apply=add
+      reshape = s32[1,2,32] reshape(rs)
+      dus = s32[2,2,32] dynamic-update-slice(destination, reshape, c1, c0, c0)
+      dot = s32[1024,1024] dot(a,b), lhs_contracting_dims={1}, rhs_contracting_dims={0}
+      ROOT tuple = tuple(dus,dot)
+    }
+  )";
+  TF_ASSERT_OK_AND_ASSIGN(std::unique_ptr<HloModule> hlo_module,
+                          ParseAndReturnVerifiedModule(hlo));
+  std::unique_ptr<HloModule> hlo_module_ref = hlo_module->Clone();
+  hlo_module->mutable_config()
+      .mutable_debug_options()
+      .set_xla_gpu_enable_dynamic_slice_fusion(true);
+  TF_ASSERT_OK_AND_ASSIGN(hlo_module,
+                          GetOptimizedModule(std::move(hlo_module)));
+  TF_ASSERT_OK_AND_ASSIGN(hlo_module_ref,
+                          GetOptimizedModule(std::move(hlo_module_ref)));
+  std::unique_ptr<HloModule> hlo_module_opt = hlo_module->Clone();
+  TF_ASSERT_OK_AND_ASSIGN(std::unique_ptr<Executable> exec,
+                          CreateExecutable(std::move(hlo_module), false));
+  GpuExecutable* gpu_exec = dynamic_cast<GpuExecutable*>(exec.get());
+  const ThunkSequence& thunks = gpu_exec->GetThunk().thunks();
+
+  // This is only needed to ensure that the next checks don't fail.
+  ASSERT_GE(thunks.size(), 6);
+
+  // In the following checks, only the order of the thunks matter.
+  EXPECT_EQ(thunks[1]->kind(), Thunk::kWaitForStreams);
+  EXPECT_EQ(thunks[2]->kind(), Thunk::kDynamicSlice);
+  EXPECT_EQ(thunks[3]->kind(), Thunk::kKernel);
+  EXPECT_EQ(thunks[4]->kind(), Thunk::kNcclReduceScatterDone);
+  EXPECT_EQ(thunks[5]->kind(), Thunk::kWaitForStreams);
+
+  // Check that the dynamic slice thunk only produces a start thunk, and not a
+  // done thunk.
+  DynamicSliceThunk* dynamic_slice_thunk =
+      dynamic_cast<DynamicSliceThunk*>(thunks[2].get());
+  ASSERT_NE(dynamic_slice_thunk, nullptr);
+  const SequentialThunk* embedded_thunk = dynamic_cast<const SequentialThunk*>(
+      dynamic_slice_thunk->embedded_thunk());
+  ASSERT_NE(embedded_thunk, nullptr);
+  ASSERT_EQ(embedded_thunk->thunks().size(), 1);
+  EXPECT_EQ(embedded_thunk->thunks()[0]->kind(),
+            Thunk::kNcclReduceScatterStart);
+
+  // Check that the offsets were propagated as constants, and not as device
+  // allocated buffers.
+  auto offsets = dynamic_slice_thunk->get_offsets();
+  auto arg_offset = offsets[0];
+  EXPECT_FALSE(arg_offset.has_value());
+  auto result_offset = offsets[1];
+  ASSERT_TRUE(result_offset.has_value());
+  ASSERT_EQ(result_offset->size(), 3);
+  uint64_t* result_offset_0 = std::get_if<uint64_t>(&result_offset->at(0));
+  uint64_t* result_offset_1 = std::get_if<uint64_t>(&result_offset->at(1));
+  uint64_t* result_offset_2 = std::get_if<uint64_t>(&result_offset->at(2));
+  ASSERT_NE(result_offset_0 && result_offset_1 && result_offset_2, false);
+  EXPECT_EQ(*result_offset_0, 1ul);
+  EXPECT_EQ(*result_offset_1, 0ul);
+  EXPECT_EQ(*result_offset_2, 0ul);
+
+  // Finally, compare the results.
+  ErrorSpec error{1e-4, 1e-4};
+  EXPECT_TRUE(RunAndCompareTwoModulesReplicated(std::move(hlo_module_ref),
+                                                std::move(hlo_module_opt),
                                                 false, true, error));
 }
 

xla/service/gpu/gpu_compiler.cc
@@ -133,6 +133,7 @@ limitations under the License.
 #include "xla/service/batchnorm_expander.h"
 #include "xla/service/buffer_assignment.h"
 #include "xla/service/call_inliner.h"
+#include "xla/service/collective_ops_utils.h"
 #include "xla/service/collective_permute_decomposer.h"
 #include "xla/service/collective_pipeliner.h"
 #include "xla/service/collective_utils.h"
@@ -166,6 +167,7 @@ limitations under the License.
 #include "xla/service/gpu/gpu_p2p_pipeliner.h"
 #include "xla/service/gpu/gpu_spmd_pipeline.h"
 #include "xla/service/gpu/hlo_fusion_stats.h"
+#include "xla/service/gpu/hlo_traversal.h"
 #include "xla/service/gpu/ir_emission_utils.h"
 #include "xla/service/gpu/ir_emitter_context.h"
 #include "xla/service/gpu/ir_emitter_unnested.h"
@@ -1296,6 +1298,23 @@ absl::Status RunDynamicSliceFusionPasses(HloModule* hlo_module,
     TF_ASSIGN_OR_RETURN(se::Platform * platform,
                         se::PlatformManager::PlatformWithId(platform_id));
     pipeline.AddPass<DynamicSliceFusionRewriter>(platform->Name());
+    pipeline.AddPass<AsyncWrapper>([](const HloInstruction* instr) {
+      absl::StatusOr<GpuBackendConfig> backend_config =
+          instr->backend_config<GpuBackendConfig>();
+      if (!backend_config.ok()) return false;
+      std::string name =
+          backend_config->fusion_backend_config().custom_fusion_config().name();
+      if (name != "address_computation" &&
+          name != "dynamic_address_computation") {
+        return false;
+      }
+      std::optional<const HloInstruction*> hero_op = HloBfsFindIf(
+          {instr->fused_instructions_computation()->root_instruction()},
+          [](const HloInstruction* instr) -> bool {
+            return IsCollective(instr);
+          });
+      return hero_op.has_value();
+    });
     TF_RETURN_IF_ERROR(pipeline.Run(hlo_module).status());
   }
 

xla/service/gpu/ir_emitter_unnested.cc
@@ -96,6 +96,7 @@ limitations under the License.
 #include "xla/service/gpu/gpu_conv_runner.h"
 #include "xla/service/gpu/gpu_norm_runner.h"
 #include "xla/service/gpu/hlo_fusion_analysis.h"
+#include "xla/service/gpu/hlo_traversal.h"
 #include "xla/service/gpu/ir_emission_utils.h"
 #include "xla/service/gpu/ir_emitter.h"
 #include "xla/service/gpu/ir_emitter_context.h"
@@ -2475,6 +2476,24 @@ absl::Status IrEmitterUnnested::EmitRecvDoneThunk(
   return absl::OkStatus();
 }
 
+// If the fusion instruction is a dynamic slice fusion instruction, with a
+// collective hero operation, then this function returns the collective
+// operation. Returns std::nullopt otherwise.
+std::optional<const HloInstruction*> GetCollectiveHeroForDynamicSliceFusion(
+    const HloFusionInstruction* instruction) {
+  absl::StatusOr<GpuBackendConfig> backend_config =
+      instruction->backend_config<GpuBackendConfig>();
+  if (!backend_config.ok()) return std::nullopt;
+  std::string name =
+      backend_config->fusion_backend_config().custom_fusion_config().name();
+  if (name != "address_computation" && name != "dynamic_address_computation") {
+    return std::nullopt;
+  }
+  return HloBfsFindIf(
+      {instruction->fused_instructions_computation()->root_instruction()},
+      [](const HloInstruction* instr) { return IsCollective(instr); });
+}
+
 absl::Status IrEmitterUnnested::EmitHloInstruction(
     const HloInstruction* instr) {
   switch (instr->opcode()) {
@@ -2508,7 +2527,27 @@ absl::Status IrEmitterUnnested::EmitHloInstruction(
           return EmitNcclAsyncDone(Thunk::kNcclAllToAllDone, instr);
         case HloOpcode::kCollectiveBroadcast:
           return EmitNcclAsyncDone(Thunk::kNcclCollectiveBroadcastDone, instr);
-        case HloOpcode::kFusion:
+        case HloOpcode::kFusion: {
+          auto collective_hero = GetCollectiveHeroForDynamicSliceFusion(
+              Cast<HloFusionInstruction>(wrapped));
+          if (collective_hero.has_value()) {
+            switch ((*collective_hero)->opcode()) {
+              case HloOpcode::kReduceScatter:
+                TF_RETURN_IF_ERROR(
+                    EmitNcclAsyncDone(Thunk::kNcclReduceScatterDone, instr));
+                break;
+              default:
+                return absl::InternalError(absl::StrFormat(
+                    "Unhandled collective in dynamic slice fusion "
+                    "instruction: %s",
+                    (*collective_hero)
+                        ->fused_instructions_computation()
+                        ->ToString()));
+            }
+          }
+          // We still want to emit the stream done thunk.
+          [[clang::fallthrough]];
+        }
         case HloOpcode::kCustomCall: {
           // Wait until the concurrent stream has finished.
           auto* async_done = Cast<HloAsyncInstruction>(instr);

openxla/openxla

xla/service/gpu/fusions/dynamic_slice_fusion_test.cc
@@ -3131,9 +3131,9 @@ TEST_F(DynamicSliceFusionTest, ReduceScatterSlice) {
   TF_ASSERT_OK_AND_ASSIGN(
       auto exec, CreateExecutable(std::move(module_new_opt_clone), false));
   GpuExecutable* gpu_exec = dynamic_cast<GpuExecutable*>(exec.get());
-  ASSERT_EQ(gpu_exec->GetThunk().thunks().size(), 2ul);
-  auto& rs_start_thunk = gpu_exec->GetThunk().thunks()[0];
-  auto& rs_done_thunk = gpu_exec->GetThunk().thunks()[1];
+  ASSERT_EQ(gpu_exec->GetThunk().thunks().size(), 4ul);
+  auto& rs_start_thunk = gpu_exec->GetThunk().thunks()[1];
+  auto& rs_done_thunk = gpu_exec->GetThunk().thunks()[2];
   ASSERT_EQ(rs_start_thunk->kind(), Thunk::kNcclReduceScatterStart);
   ASSERT_EQ(rs_done_thunk->kind(), Thunk::kNcclReduceScatterDone);
 
@@ -3267,6 +3267,91 @@ TEST_F(DynamicSliceFusionTest,
   EXPECT_TRUE(RunAndCompareTwoModulesReplicated(
       /*module_0=*/std::move(module_ref), /*module_1=*/std::move(module_opt),
       /*run_hlo_passes=*/false, /*use_threads=*/true, error));
+}
+
+TEST_F(DynamicSliceFusionTest, AsyncCollective) {
+  const char* hlo = R"(
+    HloModule test, replica_count=2
+    add {
+      x = s32[] parameter(0)
+      y = s32[] parameter(1)
+      ROOT add = s32[] add(x, y)
+    }
+    ENTRY main {
+      destination = s32[2,2,32] parameter(0)
+      c1 = s32[] constant(1)
+      c0 = s32[] constant(0)
+      c4 = s32[] constant(4)
+      source = s32[8,32] parameter(1)
+      a = s32[1024,1024] parameter(2)
+      b = s32[1024,1024] parameter(3)
+      slice = s32[4,32] slice(source), slice={[4:8], [0:32]}
+      rs = s32[2,32] reduce-scatter(slice), replica_groups={{0,1}}, dimensions={0}, to_apply=add
+      reshape = s32[1,2,32] reshape(rs)
+      dus = s32[2,2,32] dynamic-update-slice(destination, reshape, c1, c0, c0)
+      dot = s32[1024,1024] dot(a,b), lhs_contracting_dims={1}, rhs_contracting_dims={0}
+      ROOT tuple = tuple(dus,dot)
+    }
+  )";
+  TF_ASSERT_OK_AND_ASSIGN(std::unique_ptr<HloModule> hlo_module,
+                          ParseAndReturnVerifiedModule(hlo));
+  std::unique_ptr<HloModule> hlo_module_ref = hlo_module->Clone();
+  hlo_module->mutable_config()
+      .mutable_debug_options()
+      .set_xla_gpu_enable_dynamic_slice_fusion(true);
+  TF_ASSERT_OK_AND_ASSIGN(hlo_module,
+                          GetOptimizedModule(std::move(hlo_module)));
+  TF_ASSERT_OK_AND_ASSIGN(hlo_module_ref,
+                          GetOptimizedModule(std::move(hlo_module_ref)));
+  std::unique_ptr<HloModule> hlo_module_opt = hlo_module->Clone();
+  TF_ASSERT_OK_AND_ASSIGN(std::unique_ptr<Executable> exec,
+                          CreateExecutable(std::move(hlo_module), false));
+  GpuExecutable* gpu_exec = dynamic_cast<GpuExecutable*>(exec.get());
+  const ThunkSequence& thunks = gpu_exec->GetThunk().thunks();
+
+  // This is only needed to ensure that the next checks don't fail.
+  ASSERT_GE(thunks.size(), 6);
+
+  // In the following checks, only the order of the thunks matter.
+  EXPECT_EQ(thunks[1]->kind(), Thunk::kWaitForStreams);
+  EXPECT_EQ(thunks[2]->kind(), Thunk::kDynamicSlice);
+  EXPECT_EQ(thunks[3]->kind(), Thunk::kKernel);
+  EXPECT_EQ(thunks[4]->kind(), Thunk::kNcclReduceScatterDone);
+  EXPECT_EQ(thunks[5]->kind(), Thunk::kWaitForStreams);
+
+  // Check that the dynamic slice thunk only produces a start thunk, and not a
+  // done thunk.
+  DynamicSliceThunk* dynamic_slice_thunk =
+      dynamic_cast<DynamicSliceThunk*>(thunks[2].get());
+  ASSERT_NE(dynamic_slice_thunk, nullptr);
+  const SequentialThunk* embedded_thunk = dynamic_cast<const SequentialThunk*>(
+      dynamic_slice_thunk->embedded_thunk());
+  ASSERT_NE(embedded_thunk, nullptr);
+  ASSERT_EQ(embedded_thunk->thunks().size(), 1);
+  EXPECT_EQ(embedded_thunk->thunks()[0]->kind(),
+            Thunk::kNcclReduceScatterStart);
+
+  // Check that the offsets were propagated as constants, and not as device
+  // allocated buffers.
+  auto offsets = dynamic_slice_thunk->get_offsets();
+  auto arg_offset = offsets[0];
+  EXPECT_FALSE(arg_offset.has_value());
+  auto result_offset = offsets[1];
+  ASSERT_TRUE(result_offset.has_value());
+  ASSERT_EQ(result_offset->size(), 3);
+  int64_t* result_offset_0 = std::get_if<int64_t>(&result_offset->at(0));
+  int64_t* result_offset_1 = std::get_if<int64_t>(&result_offset->at(1));
+  int64_t* result_offset_2 = std::get_if<int64_t>(&result_offset->at(2));
+  ASSERT_NE(result_offset_0 && result_offset_1 && result_offset_2, false);
+  EXPECT_EQ(*result_offset_0, 1l);
+  EXPECT_EQ(*result_offset_1, 0l);
+  EXPECT_EQ(*result_offset_2, 0l);
+
+  // Finally, compare the results.
+  ErrorSpec error{1e-4, 1e-4};
+  EXPECT_TRUE(RunAndCompareTwoModulesReplicated(std::move(hlo_module_ref),
+                                                std::move(hlo_module_opt),
+                                                false, true, error));
 }
 
 }  // namespace

xla/service/gpu/gpu_compiler.cc
@@ -124,6 +124,7 @@ limitations under the License.
 #include "xla/hlo/transforms/simplifiers/tuple_simplifier.h"
 #include "xla/hlo/transforms/simplifiers/zero_sized_hlo_elimination.h"
 #include "xla/hlo/transforms/while_loop_trip_count_annotator.h"
+#include "xla/hlo/utils/hlo_traversal.h"
 #include "xla/maybe_owning.h"
 #include "xla/service/all_reduce_promotion.h"
 #include "xla/service/all_reduce_reassociate.h"
@@ -132,6 +133,7 @@ limitations under the License.
 #include "xla/service/batchnorm_expander.h"
 #include "xla/service/buffer_assignment.h"
 #include "xla/service/call_inliner.h"
+#include "xla/service/collective_ops_utils.h"
 #include "xla/service/collective_permute_decomposer.h"
 #include "xla/service/collective_pipeliner.h"
 #include "xla/service/collective_utils.h"
@@ -1300,6 +1302,23 @@ absl::Status RunDynamicSliceFusionPasses(HloModule* hlo_module,
     TF_ASSIGN_OR_RETURN(se::Platform * platform,
                         se::PlatformManager::PlatformWithId(platform_id));
     pipeline.AddPass<DynamicSliceFusionRewriter>(platform->Name());
+    pipeline.AddPass<AsyncWrapper>([](const HloInstruction* instr) {
+      absl::StatusOr<GpuBackendConfig> backend_config =
+          instr->backend_config<GpuBackendConfig>();
+      if (!backend_config.ok()) return false;
+      std::string name =
+          backend_config->fusion_backend_config().custom_fusion_config().name();
+      if (name != "address_computation" &&
+          name != "dynamic_address_computation") {
+        return false;
+      }
+      std::optional<const HloInstruction*> hero_op = HloBfsFindIf(
+          {instr->fused_instructions_computation()->root_instruction()},
+          [](const HloInstruction* instr) -> bool {
+            return IsCollective(instr);
+          });
+      return hero_op.has_value();
+    });
     TF_RETURN_IF_ERROR(pipeline.Run(hlo_module).status());
   }
 

xla/service/gpu/ir_emitter_unnested.cc
@@ -80,6 +80,7 @@ limitations under the License.
 #include "xla/hlo/ir/hlo_instructions.h"
 #include "xla/hlo/ir/hlo_opcode.h"
 #include "xla/hlo/ir/hlo_schedule.h"
+#include "xla/hlo/utils/hlo_traversal.h"
 #include "xla/layout.h"
 #include "xla/layout_util.h"
 #include "xla/literal.h"
@@ -2514,6 +2515,24 @@ absl::Status IrEmitterUnnested::EmitRecvDoneThunk(
   return absl::OkStatus();
 }
 
+// If the fusion instruction is a dynamic slice fusion instruction, with a
+// collective hero operation, then this function returns the collective
+// operation. Returns std::nullopt otherwise.
+std::optional<const HloInstruction*> GetCollectiveHeroForDynamicSliceFusion(
+    const HloFusionInstruction* instruction) {
+  absl::StatusOr<GpuBackendConfig> backend_config =
+      instruction->backend_config<GpuBackendConfig>();
+  if (!backend_config.ok()) return std::nullopt;
+  std::string name =
+      backend_config->fusion_backend_config().custom_fusion_config().name();
+  if (name != "address_computation" && name != "dynamic_address_computation") {
+    return std::nullopt;
+  }
+  return HloBfsFindIf(
+      {instruction->fused_instructions_computation()->root_instruction()},
+      [](const HloInstruction* instr) { return IsCollective(instr); });
+}
+
 absl::Status IrEmitterUnnested::EmitHloInstruction(
     const HloInstruction* instr) {
   switch (instr->opcode()) {
@@ -2549,7 +2568,27 @@ absl::Status IrEmitterUnnested::EmitHloInstruction(
           return EmitNcclAsyncDone(Thunk::kNcclRaggedAllToAllDone, instr);
         case HloOpcode::kCollectiveBroadcast:
           return EmitNcclAsyncDone(Thunk::kNcclCollectiveBroadcastDone, instr);
-        case HloOpcode::kFusion:
+        case HloOpcode::kFusion: {
+          auto collective_hero = GetCollectiveHeroForDynamicSliceFusion(
+              Cast<HloFusionInstruction>(wrapped));
+          if (collective_hero.has_value()) {
+            switch ((*collective_hero)->opcode()) {
+              case HloOpcode::kReduceScatter:
+                TF_RETURN_IF_ERROR(
+                    EmitNcclAsyncDone(Thunk::kNcclReduceScatterDone, instr));
+                break;
+              default:
+                return absl::InternalError(absl::StrFormat(
+                    "Unhandled collective in dynamic slice fusion "
+                    "instruction: %s",
+                    (*collective_hero)
+                        ->fused_instructions_computation()
+                        ->ToString()));
+            }
+          }
+          // We still want to emit the stream done thunk.
+          [[clang::fallthrough]];
+        }
         case HloOpcode::kCustomCall: {
           // Wait until the concurrent stream has finished.
           auto* async_done = Cast<HloAsyncInstruction>(instr);